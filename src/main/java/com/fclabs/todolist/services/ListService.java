650f9728c9aad3f8e14e391f276a4a159ad698cc
Janderson Cardoso <jandersonfc.com@gmail.com>
2022-04-18 18:40:04 -0300

    Implements ListService methods with unit tests

--- src/main/java/com/fclabs/todolist/services/ListService.java
+++ src/main/java/com/fclabs/todolist/services/ListService.java
@@ -1,11 +1,12 @@
 package com.fclabs.todolist.services;
 
+import java.util.List;
+
 import com.fclabs.todolist.entities.ListEntity;
 import com.fclabs.todolist.repositories.ListRepository;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
 
 @Service
 public class ListService {
@@ -15,6 +16,18 @@ public class ListService {
 
     public List<ListEntity> getAll() {
         return listRepository.findAll();
+    }
+
+    public ListEntity getById(Long id) {
+        return listRepository.getById(id);
+    }
+
+    public ListEntity save(ListEntity listEntity) {
+        return listRepository.save(listEntity);
+    }
+
+    public void delete(Long id) {
+        listRepository.deleteById(id);
     }
 
 }
--- src/test/java/com/fclabs/todolist/services/ListServicesTest.java
+++ src/test/java/com/fclabs/todolist/services/ListServicesTest.java
@@ -1,18 +1,25 @@
 package com.fclabs.todolist.services;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.Collections;
+import java.util.List;
+
+import javax.persistence.EntityNotFoundException;
+
 import com.fclabs.todolist.entities.ListEntity;
 import com.fclabs.todolist.repositories.ListRepository;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
-
-import java.util.Collections;
-import java.util.List;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.*;
 
 public class ListServicesTest {
 
@@ -36,6 +43,58 @@ public class ListServicesTest {
 
         verify(listRepository, times(1)).findAll();
         assertEquals(expectedEntities, responseEntities);
+    }
+
+    @Test
+    public void test_getById_withSuccess() {
+        ListEntity listEntity = buildList("test");
+        listEntity.setId(102030);
+
+        when(listRepository.getById(listEntity.getId())).thenReturn(listEntity);
+
+        ListEntity response = listService.getById(listEntity.getId());
+
+        verify(listRepository, times(1)).getById(listEntity.getId());
+
+        assertEquals(listEntity, response);
+    }
+
+    @Test
+    public void test_getById_whenNotFound() {
+        ListEntity listEntity = buildList("test");
+        listEntity.setId(102030);
+
+        when(listRepository.getById(listEntity.getId())).thenThrow(new EntityNotFoundException("Fake Error"));
+
+        Exception exception = assertThrows(EntityNotFoundException.class, () -> {
+            listService.getById(listEntity.getId());
+        });
+
+        verify(listRepository, times(1)).getById(listEntity.getId());
+        assertEquals("Fake Error", exception.getMessage());
+    }
+
+    @Test
+    public void test_save() {
+        ListEntity listEntityNew = buildList("test new");
+        ListEntity listEntitySaved = buildList("test saved");
+
+        when(listRepository.save(listEntityNew)).thenReturn(listEntitySaved);
+
+        ListEntity response = listService.save(listEntityNew);
+
+        verify(listRepository, times(1)).save(listEntityNew);
+
+        assertEquals(response, listEntitySaved);
+    }
+
+    @Test
+    public void test_delete() {
+        doNothing().when(listRepository).deleteById(102030L);
+
+        listService.delete(102030L);
+
+        verify(listRepository, times(1)).deleteById(102030L);
     }
 
     private ListEntity buildList(String name) {
